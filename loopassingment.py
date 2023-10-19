8aafac8aa71215144c66804dae9f25dd86058f19
Jamahdd <144923650+Jamahdd@users.noreply.github.com>
2023-10-19 16:17:41 +0000

    first commit

---
+++ loopassingment.py
@@ -0,0 +1,13 @@
+# Python While- Allows us to repeat blocks 
+# of code indefinitely, unless told otherwise. 
+
+# how do we write a while loop:
+
+# while loop = a keyword that allows us to 
+# repeat a block of code indefinitely/ continuously/ 
+# infinitely.
+
+i= 0
+while i < 10:
+  print('hello')
+  i+=1
